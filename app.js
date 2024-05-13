fa13ee7be1c7ed059209ae3d5c8abd0d1f598c75
Garvit1809 <garvitvarshne18@gmail.com>
2024-05-13 19:46:18 +0530

    changing cors policy

--- app.js
+++ app.js
@@ -12,7 +12,27 @@ dotenv.config({ path: "./.env" });
 
 const app = express();
 
-app.use(cors());
+// List of allowed origins
+const allowedOrigins = [
+    'https://micro-finance-portal.netlify.app',
+    'https://shimmering-begonia-efc2fa.netlify.app'
+];
+
+// CORS options delegate
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+// Apply CORS dynamically based on the origin of the request
+app.use(cors(corsOptionsDelegate));
+
+// app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 // routes
