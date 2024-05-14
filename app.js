735479a2ce499314e9b376df03443fec96f8a45d
Garvit1809 <garvitvarshne18@gmail.com>
2024-05-14 19:46:19 +0530

    adding cors import

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const globalErrorHandler = require("./controller/errorController");
+const AppError = require("./utils/appError");
 
 const userRouter = require("./routes/userRoutes");
 const staffRouter = require("./routes/staffRoutes");
@@ -12,30 +13,29 @@ dotenv.config({ path: "./.env" });
 
 const app = express();
 
-// List of allowed origins
-const allowedOrigins = [
-    'https://micro-finance-por-tal.netlify.app',
-    'https://micro-finance-server.vercel.app/api/v1/staff/login',
-    'https://shimmering-begonia-efc2fa.netlify.app',
-    "http://localhost:5173",
-    "http://localhost:3000"
-];
+// // List of allowed origins
+// const allowedOrigins = [
+//     'https://micro-finance-por-tal.netlify.app',
+//     'https://shimmering-begonia-efc2fa.netlify.app',
+//     "http://localhost:5173",
+//     "http://localhost:3000"
+// ];
 
-// // CORS options delegate
-const corsOptionsDelegate = function (req, callback) {
-    let corsOptions;
-    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-    } else {
-        corsOptions = { origin: false }; // disable CORS for this request
-    }
-    callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// // // CORS options delegate
+// const corsOptionsDelegate = function (req, callback) {
+//     let corsOptions;
+//     if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+//         corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//     } else {
+//         corsOptions = { origin: false }; // disable CORS for this request
+//     }
+//     callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
-// Apply CORS dynamically based on the origin of the request
-app.use(cors(corsOptionsDelegate));
+// // Apply CORS dynamically based on the origin of the request
+// app.use(cors(corsOptionsDelegate));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 // routes
@@ -51,7 +51,6 @@ app.all("*", (req, res, next) => {
 });
 
 app.use(globalErrorHandler);
-
 
 mongoose
     .connect(process.env.MONGO_URI)
